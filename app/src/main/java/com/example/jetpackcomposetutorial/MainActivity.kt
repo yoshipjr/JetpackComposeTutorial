5ad1c93e98321edb0cf50fae84b8d23181481336
yoshipjr <yoshihisa4413kitahara@gmail.com>
2022-07-30 17:47:17 +0900

    アニメーションを追加

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -2,68 +2,35 @@ package com.example.jetpackcomposetutorial
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.os.Message
+import com.example.jetpackcomposetutorial.dataSource.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposeCompilerApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposetutorial.dataSource.SampleData
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
+import com.example.jetpackcomposetutorial.widget.MessageCard
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTutorialTheme {
-                MessageCard(Message("Android", "JetpackComose"))
+                Conversation(messages = SampleData.conversationSample)
             }
         }
     }
 }
 
-data class Message(val auther: String, val body: String)
-
 @Composable
-fun MessageCard(msg: com.example.jetpackcomposetutorial.Message) {
-    Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                // Set image size to 40 dp
-                .size(40.dp)
-                // Clip image to be shaped as a circle
-                .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
-        )
-        Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(
-                text = msg.auther,
-                color = MaterialTheme.colors.secondaryVariant
-            )
-            Spacer(modifier = Modifier.height(4.dp))
-            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.body2
-                )
-            }
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
         }
     }
-
 }
 
 @Preview(name = "Light Mode")
@@ -72,9 +39,10 @@ fun MessageCard(msg: com.example.jetpackcomposetutorial.Message) {
     showBackground = true,
     name = "Dark Mode"
 )
+
 @Composable
-fun PreviewMessageCard() {
+fun PreviewConversation() {
     JetpackComposeTutorialTheme {
-        MessageCard(Message("Colleague", "hello"))
+        Conversation(messages = SampleData.conversationSample)
     }
 }
